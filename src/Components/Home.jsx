766f5afc349190d4a8cb9cda7ef44df296866429
jawad Bin Azam <abidibnazam@gmail.com>
2023-04-10 16:23:08 +0600

    catagory list done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
     console.log(jobs);
     return (
         <div>
+            <section className="">
             <div className="grid md:grid-cols-2 lg:grid-cols-2">
                 {/* left portion: text */}
                 <div className="mt-36">
@@ -27,9 +28,48 @@ const Home = () => {
                     <img src="/src/assets/hero-user-img.gif" alt="" className="" />
                 </div>
             </div> 
+            </section>
+           
             {/* end of hero section */}
 
             {/* starting of job catagory */}
+
+            <div className="">
+
+                <div className="text-center mt-32">
+                    <h1 className="text-5xl font-bold text-slate-800">Job Category List</h1>
+                    <p className="mt-4">Explore thousands of job opportunities with all the information you need. Its your future</p>
+                </div>
+                {/* available jobs */}
+                <div className="grid md:grid-cols-4 lg:grid-cols-4 gap-6 mt-8">
+
+                    <div className="jobs-card">
+                        <img className="jobs-icon" src="/src/assets/Icons/accounts 1.png" alt=""/>
+                        <h1 className="mt-8 font-bold">Account & Finance</h1>
+                        <small className="small-text">300 Jobs Available</small>
+                    </div>
+
+                    <div className="jobs-card">
+                        <img className="jobs-icon" src="/src/assets/Icons/business 1.png" alt=""/>
+                        <h1 className="mt-8 font-bold">Creative Design</h1>
+                        <small className="small-text">100+ Jobs Available</small>
+                    </div>
+
+                    <div className="jobs-card">
+                        <img className="jobs-icon" src="/src/assets/Icons/social-media 1.png" alt=""/>
+                        <h1 className="mt-8 font-bold">Marketing & Sales</h1>
+                        <small className="small-text">150 Jobs Available</small>
+                    </div>
+
+                    <div className="jobs-card">
+                        <img className="jobs-icon" src="/src/assets/Icons/chip 1.png" alt=""/>
+                        <h1 className="mt-8 font-bold">Engineering Job</h1>
+                        <small className="small-text">224 Jobs Available</small>
+                    </div>
+
+                </div>
+
+            </div>
             
         </div>
     );
