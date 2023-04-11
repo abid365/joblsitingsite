42d3134cbad3136676ca6e135a2cafc3e21865f5
jawad Bin Azam <abidibnazam@gmail.com>
2023-04-12 00:46:14 +0600

    chart added

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import {
   AreaChart,
   Area,
@@ -12,62 +13,58 @@ import {
 const data = [
   {
     name: "Assignment 1",
-    marks: 57,
-    
+    marks: 57
   },
   {
     name: "Assignment 2",
-    marks: 59,
-    
+    marks: 59
   },
   {
     name: "Assignment 3",
-    marks: 50,
-    
+    marks: 50
   },
   {
     name: "Assignment 4",
-    marks: 27,
-    
+    marks: 27
   },
   {
     name: "Assignment 5",
-    marks: 55,
-    
+    marks: 55
   },
   {
     name: "Assignment 6",
-    marks: 53,
-  
+    marks: 53
   },
   {
     name: "Assignment 7",
-    marks: 60,
-    
+    marks: 60
   }
 ];
 
 const Stats = () => {
   return (
-    <ResponsiveContainer width="100%" height="60%">
-     <AreaChart
-      width={100}
-      height={400}
-      data={data}
-      margin={{
-        top: 10,
-        right: 30,
-        left: 0,
-        bottom: 0
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Area type="monotone" dataKey="marks" stroke="#8884d8" fill="#8884d8" />
-    </AreaChart>
-  </ResponsiveContainer>
+    <div className="">
+      <ResponsiveContainer width="100%" height={500}>
+          <AreaChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                  top: 10,
+                  right: 30,
+                  left: 0,
+                  bottom: 0
+                }}
+              >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Area type="monotone" dataKey="marks" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+    </ResponsiveContainer>
+    </div>
+    
     
   );
 };
