c275cc99f32ae877f9f8b474cd6a87c981dd7261
jawad Bin Azam <abidibnazam@gmail.com>
2023-04-11 23:27:01 +0600

    added chart

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -1,91 +1,75 @@
 import React from 'react';
 import {
-    Radar,
-    RadarChart,
-    PolarGrid,
-    PolarAngleAxis,
-    PolarRadiusAxis
-  } from "recharts";
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer
+} from "recharts";
 
-  const data = [
-    {
-      subject: "A1",
-      A: 58,
-      B: 110,
-      fullMark: 60
-    },
-    {
-      subject: "A2",
-      A: 59,
-      B: 130,
-      fullMark: 60
-    },
-    {
-      subject: "A3",
-      A: 50,
-      B: 130,
-      fullMark: 60
-    },
-    {
-      subject: "A4",
-      A: 27,
-      B: 100,
-      fullMark: 60
-    },
-    {
-      subject: "A5",
-      A: 55,
-      B: 90,
-      fullMark: 60
-    },
-    {
-      subject: "A6",
-      A: 53,
-      B: 85,
-      fullMark: 60
-    },
-    {
-        subject: "A7",
-        A: 60,
-        B: 85,
-        fullMark: 60
-      }
-  ];
+const data = [
+  {
+    name: "Assignment 1",
+    marks: 57,
+    
+  },
+  {
+    name: "Assignment 2",
+    marks: 59,
+    
+  },
+  {
+    name: "Assignment 3",
+    marks: 50,
+    
+  },
+  {
+    name: "Assignment 4",
+    marks: 27,
+    
+  },
+  {
+    name: "Assignment 5",
+    marks: 55,
+    
+  },
+  {
+    name: "Assignment 6",
+    marks: 53,
+  
+  },
+  {
+    name: "Assignment 7",
+    marks: 60,
+    
+  }
+];
 
 const Stats = () => {
-    return (
-
-        <div className='grid lg:grid-cols-2'>
-            <RadarChart
-                cx={300}
-                cy={250}
-                outerRadius={100}
-                width={500}
-                height={500}
-                data={data}
-              >
-              <PolarGrid />
-              <PolarAngleAxis dataKey="subject" />
-              <PolarRadiusAxis />
-              <Radar
-                name="Mike"
-                dataKey="A"
-                stroke="#8884d8"
-                fill="#8884d8"
-                fillOpacity={0.6}
-              />
-            </RadarChart>
-            <div className="text-center p-10">
-              <h1>Assignment1:58</h1>
-              <h1>Assignment2:59</h1>
-              <h1>Assignment3:50</h1>
-              <h1>Assignment4:27</h1>
-              <h1>Assignment5:55</h1>
-              <h1>Assignment6:53</h1>
-              <h1>Assignment7:60</h1>
-            </div>
-        </div>
-    );
+  return (
+    <ResponsiveContainer width="100%" height="60%">
+     <AreaChart
+      width={100}
+      height={400}
+      data={data}
+      margin={{
+        top: 10,
+        right: 30,
+        left: 0,
+        bottom: 0
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Area type="monotone" dataKey="marks" stroke="#8884d8" fill="#8884d8" />
+    </AreaChart>
+  </ResponsiveContainer>
+    
+  );
 };
 
 export default Stats;
