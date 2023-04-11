11cadec7bbc0330e554c09d6fc3172f93a3fc575
jawad Bin Azam <abidibnazam@gmail.com>
2023-04-11 20:23:32 +0600

    added blog

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -3,7 +3,30 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h1 className="">Blogs will be written soon</h1>
+            <div className="mt-12">
+                <div className='border rounded-lg p-4 bg-indigo-200'>
+                    <h1 className="my-3 font-bold text-slate-600">When should you use context api?</h1>
+                    <p className="text-sm">React Context is a way for a child component to access a value in a parent component.
+                    React Context solves the problem of props drilling. It allows you to share props or state with an indirect child or parent.
+                    </p>
+                </div>
+                <div className='border rounded-lg p-4 bg-indigo-200 mt-5'>
+                    <h1 className="my-3 font-bold text-slate-600">What is custom hook?</h1>
+                    <p className="text-sm">Custom Hook is a JavaScript function which we create by ourselves, when we want to share logic between other JavaScript functions. It allows you to reuse some piece of code in several parts of your app.
+                    </p>
+                </div>
+                <div className='border rounded-lg p-4 bg-indigo-200 mt-5'>
+                    <h1 className="my-3 font-bold text-slate-600">What is useRef?</h1>
+                    <p className="text-sm">The useRef Hook is a function that returns a mutable ref object whose .current property is initialized with the passed argument (initialValue). The returned object will persist for the full lifetime of the component.
+                    </p>
+                </div>
+                <div className='border rounded-lg p-4 bg-indigo-200 mt-5'>
+                    <h1 className="my-3 font-bold text-slate-600">What is useMemo?</h1>
+                    <p className="text-sm"><code>useMemo(() =&gt; computation(a, b), [a, b])</code> is the hook that lets you memoize expensive computations. Given the same [a, b] dependencies, once memoized, the hook is going to return the memoized value without invoking computation(a, b).
+                    </p>
+                </div>
+               
+            </div>
         </div>
     );
 };
--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -1,9 +1,89 @@
 import React from 'react';
+import {
+    Radar,
+    RadarChart,
+    PolarGrid,
+    PolarAngleAxis,
+    PolarRadiusAxis
+  } from "recharts";
+
+  const data = [
+    {
+      subject: "A1",
+      A: 58,
+      B: 110,
+      fullMark: 60
+    },
+    {
+      subject: "A2",
+      A: 59,
+      B: 130,
+      fullMark: 60
+    },
+    {
+      subject: "A3",
+      A: 50,
+      B: 130,
+      fullMark: 60
+    },
+    {
+      subject: "A4",
+      A: 27,
+      B: 100,
+      fullMark: 60
+    },
+    {
+      subject: "A5",
+      A: 55,
+      B: 90,
+      fullMark: 60
+    },
+    {
+      subject: "A6",
+      A: 53,
+      B: 85,
+      fullMark: 60
+    },
+    {
+        subject: "A7",
+        A: 60,
+        B: 85,
+        fullMark: 60
+      }
+  ];
 
 const Stats = () => {
     return (
-        <div>
-            <h1 className="">This is stats</h1>
+
+        <div className='grid lg:grid-cols-2'>
+            <RadarChart
+                cx={300}
+                cy={250}
+                outerRadius={100}
+                width={500}
+                height={500}
+                data={data}
+              >
+              <PolarGrid />
+              <PolarAngleAxis dataKey="subject" />
+              <PolarRadiusAxis />
+              <Radar
+                name="Mike"
+                dataKey="A"
+                stroke="#8884d8"
+                fill="#8884d8"
+                fillOpacity={0.6}
+              />
+            </RadarChart>
+            <div className="text-center p-10">
+              <h1>Assignment1:58</h1>
+              <h1>Assignment2:59</h1>
+              <h1>Assignment3:50</h1>
+              <h1>Assignment4:27</h1>
+              <h1>Assignment5:55</h1>
+              <h1>Assignment6:53</h1>
+              <h1>Assignment7:60</h1>
+            </div>
         </div>
     );
 };
